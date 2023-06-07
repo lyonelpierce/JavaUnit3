1b909a15be9c10e110e08770c58f9640ff719414
Lyonel Pierce <lyonel@live.com>
2023-06-07 18:41:08 -0400

    Unit 3

---
+++ src/test/java/com/javaunit3/springmvc/Javaunit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaunit3.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaunit3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
